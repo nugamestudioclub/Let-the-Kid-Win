e07b965b89c3cee47aa355f1b0a8d100add4b2b8
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-02-26 01:13:33 -0500

    added snake interpolation

--- Assets/Scripts/Transporter.cs
+++ Assets/Scripts/Transporter.cs
@@ -4,54 +4,53 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public class Transporter : MonoBehaviour
-{
-    [SerializeField]
-    private int startIndex;
-    public int StartIndex { get => startIndex; }
-    [SerializeField]
-    private int endIndex;
-    public int EndIndex { get => endIndex; }
+public class Transporter : MonoBehaviour {
+	[SerializeField]
+	private int startIndex;
+	public int StartIndex { get => startIndex; }
+	[SerializeField]
+	private int endIndex;
+	public int EndIndex { get => endIndex; }
 
-    [SerializeField]
-    private List<Transform> points = new();
-    public List<Transform> Points => points;
+	[SerializeField]
+	private List<Transform> points = new();
+	public List<Transform> Points => points;
 
-    private List<float> distanceBetweenPoints = new();
+	private List<float> distanceBetweenPoints = new();
 
-    [SerializeField]
-    private float gizmoPointSize = 0.25f;
+	[SerializeField]
+	private float gizmoPointSize = 0.25f;
 
-    [SerializeField]
-    private int transportTime;
-    public int TransportTime { get => transportTime; }
+	[SerializeField]
+	private int transportTime;
+	public int TransportTime { get => transportTime; }
 
-    private void Awake()
-    {
-        CalculateDistanceBetweenPoints();
-    }
+	private void Awake() {
+		CalculateDistanceBetweenPoints();
+	}
 
-    private void OnDrawGizmosSelected()
-    {
-        foreach (var point in points)
-        {
-            Gizmos.color = Color.white;
-            Gizmos.DrawWireSphere(point.position, gizmoPointSize);
-        }
-    }
-    private void CalculateDistanceBetweenPoints()
-    {
-        List<Vector2> pointPositions = points.Select(p => (Vector2)p.position).ToList();
-        for (int i = 0; i < pointPositions.Count - 1; i++)
-        {
-            distanceBetweenPoints.Add(Vector2.Distance(pointPositions[i], pointPositions[i + 1]));
-        }
-    }
-    public float DistanceToNextPoint(int index)
-    {
-        return distanceBetweenPoints[index];
-    }
-    public float TotalLength => distanceBetweenPoints.Sum();
+	private void OnDrawGizmosSelected() {
+		foreach( var point in points ) {
+			Gizmos.color = Color.white;
+			Gizmos.DrawWireSphere(point.position, gizmoPointSize);
+		}
+		var curvePoints = points.Select(t => t.position).ToArray();
+
+		Gizmos.color = Color.blue;
+		foreach( var point in VectorMath.InterpolateCurve(curvePoints, 3, 4) )
+			Gizmos.DrawWireSphere(point, gizmoPointSize / 2);
+	}
+
+	private void CalculateDistanceBetweenPoints() {
+		List<Vector2> pointPositions = points.Select(p => (Vector2)p.position).ToList();
+		for( int i = 0; i < pointPositions.Count - 1; i++ ) {
+			distanceBetweenPoints.Add(Vector2.Distance(pointPositions[i], pointPositions[i + 1]));
+		}
+	}
+	public float DistanceToNextPoint(int index) {
+		return distanceBetweenPoints[index];
+	}
+	public float TotalLength => distanceBetweenPoints.Sum();
 
 
 }
---
+++ Assets/Scripts/VectorMath.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class VectorMath {
+	private static Vector3 InterpolateX(IList<Vector3> points, float x) {
+		float y = 0f;
+		for( int i = 0; i < points.Count; ++i ) {
+			float term = points[i].y;
+			for( int j = 0; j < points.Count; ++j )
+				if( i != j )
+					term *= (x - points[j].x) / (points[i].x - points[j].x);
+			y += term;
+		}
+		return new(x, y);
+	}
+
+	private static Vector3 InterpolateY(IList<Vector3> points, float y) {
+		float x = 0f;
+		for( int i = 0; i < points.Count; ++i ) {
+			float term = points[i].x;
+			for( int j = 0; j < points.Count; ++j )
+				if( i != j )
+					term *= (y - points[j].y) / (points[i].y - points[j].y);
+			x += term;
+		}
+		return new(x, y);
+	}
+
+	public static IEnumerable<Vector3> InterpolateSegment(IList<Vector3> points, int samples) {
+		var start = points[0];
+		var end = points[^1];
+		var delta = new Vector3(end.x - start.x, end.y - start.y);
+
+		if( Math.Abs(delta.x) > Math.Abs(delta.y) ) {
+			float epsilon = delta.x / samples;
+			yield return start;
+			for( int i = 1; i < samples; ++i )
+				yield return InterpolateX(points, start.x + (i * epsilon));
+		}
+		else {
+			yield return start;
+			float epsilon = delta.y / samples;
+			for( int i = 1; i < samples; ++i )
+				yield return InterpolateY(points, start.y + (i * epsilon));
+		}
+	}
+
+
+	/*
+	(n - 1) / (k - 1)
+
+	1, 1:  0
+
+	2, 1:  0 1
+
+	3, 1:  0 1 2
+
+	4, 1:  0 1 2 3
+
+	5, 2:  0 1 2
+	           2 3 4
+
+	6, 2:  0 1 2
+	           2 3 4 5
+
+	7, 3:  0 1 2
+	           2 3 4
+	               4 5 6
+	8, 3:  0 1 2
+	           2 3 4
+	               4 5 6 7
+
+	9, 4:  0 1 2
+	           2 3 4
+	               4 5 6
+	                   6 7 8
+	10, 4: 0 1 2
+	           2 3 4
+	               4 5 6
+	                   6 7 8 9
+	11, 5: 0 1 2
+	           2 3 4
+	               4 5 6
+	                   6 7 8
+	                       8 9 10
+	 */
+
+	public static IEnumerable<Vector3> InterpolateCurve(IList<Vector3> points, int segmentLength, int samplesPerSegment) {
+		var sourcePoints = points.ToList();
+		int segments = (points.Count - 1) / (segmentLength - 1);
+		for( int i = 0; i < segments - 1; ++i ) {
+			foreach( var point in InterpolateSegment(
+				sourcePoints.GetRange(i * (segmentLength - 1), segmentLength), samplesPerSegment)
+				)
+				yield return point;
+			foreach( var point in InterpolateSegment(
+				sourcePoints.GetRange(segments * (segmentLength - 1), sourcePoints.Count - segments * (segmentLength - 1)), samplesPerSegment)
+				)
+				yield return point;
+			yield return points[^1];
+		}
+	}
+}
