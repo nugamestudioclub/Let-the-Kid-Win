e96221879e84031b56e636d68e23bb98ed6e3ae7
sudzoof <sunjoshua2@gmail.com>
2023-02-25 21:28:19 -0500

    Made stuff actually accessible to other classes lmao

--- Assets/Scripts/Spinner.cs
+++ Assets/Scripts/Spinner.cs
@@ -8,14 +8,14 @@ public class Spinner : MonoBehaviour
     private GameObject _pivot;
 
     [SerializeField]
-    private float _rotationSpeedMultiplier = 100.0f;
+    private float _rotationSpeedMultiplier = 1500.0f;
 
     [SerializeField]
-    private float _decelerationSpeed = 1.0f;
+    private float _decelerationSpeed = 300.0f;
 
     private float _rotationSpeed;
 
-    private bool _spinFinished = true;
+    public bool spinFinished { get; private set; } = true;
 
     // Start is called before the first frame update
     void Start()
@@ -27,29 +27,24 @@ public class Spinner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!_spinFinished)
+        if (!spinFinished)
         {
             _pivot.transform.Rotate(0, 0, _rotationSpeed * Time.deltaTime);
             _rotationSpeed -= _decelerationSpeed * Time.deltaTime;
             if (_rotationSpeed < 0)
             {
-                _spinFinished = true;
+                spinFinished = true;
             }
         }
     }
 
-    void Spin(float power)
+    public void Spin(float power)
     {
-        _spinFinished = false;
+        spinFinished = false;
         _rotationSpeed = _rotationSpeedMultiplier * power;
     }
 
-    bool IsSpinFinished()
-    {
-        return _spinFinished;
-    }
-
-    int GetSegment()
+    public int GetSegment()
     {
         float rotation = _pivot.transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
         int segment = Mathf.Abs(Mathf.FloorToInt(rotation / (Mathf.PI / 3)) - 6) % 6 + 1;
