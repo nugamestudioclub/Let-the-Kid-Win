702c3d8e67184ce62d6385cf7cc49d5fde9cd322
Sebastian Kobori <sjkobori@gmail.com>
2023-02-27 19:59:21 -0500

    Continuing quest system

---
+++ Assets/Scripts/Quest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest
+{
+
+    private readonly Func<GameGlobals, bool> IsCompleteCallback;
+
+    public Quest(Func<GameGlobals, bool> IsCompleteCallback)
+    {
+        this.IsCompleteCallback = IsCompleteCallback;
+    }
+    public bool IsComplete(GameGlobals gameGlobals)
+    {
+        return IsCompleteCallback(gameGlobals);
+    }
+}
