6ece4d340b9ae7c30a453861a8f9e1aae96360dd
sudzoof <sunjoshua2@gmail.com>
2023-03-04 21:33:50 -0500

    Power bar now charges on AI turn

--- Assets/Scripts/AIPlayerController.cs
+++ Assets/Scripts/AIPlayerController.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class AIPlayerController : PlayerController {
 	public override int PlayerID => (int)Player.Child;
 
+	private float power;
+	private bool isCharging = false;
+
 	protected override void DoTakeTurn() {
 		IsTakingTurn = true;
-		float power = Random.Range(0.5f, 1f);
-		GameState.Instance.Spinner.Spin(power);
+		isCharging = true;
+		power = Random.Range(0.3f, 1f);
 	}
 
 	protected override void DoUpdate() {
 		var gameState = GameState.Instance;
-		if( IsTakingTurn && gameState.Spinner.spinFinished ) {
+		if (IsTakingTurn)
+        {
+			if (isCharging)
+            {
+				gameState.ChargePower.Tick();
+				if (gameState.ChargePower.GetCharge() > power)
+                {
+					isCharging = false;
+					GameState.Instance.Spinner.Spin(power);
+				}
+            }
+			else if (gameState.Spinner.spinFinished)
+            {
+				IsTakingTurn = false;
+				gameState.ChargePower.ResetCharge();
+				int spaces = gameState.Spinner.GetSegment();
+				gameState.Globals.LastRoll = spaces;
+				gameState.Board.MovePlayer(PlayerID, spaces);
+			}
+        }
+
+
+		/* if( IsTakingTurn && gameState.Spinner.spinFinished ) {
 			IsTakingTurn = false;
 			int spaces = gameState.Spinner.GetSegment();
 			gameState.Globals.LastRoll = spaces;
 			gameState.Board.MovePlayer(PlayerID, spaces);
-		}
+		} */
 	}
 }
