850e67140eb87325453ebd710783375950a42c3c
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2023-03-05 00:12:11 -0500

    tweaked snake and ladder animations

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ladder : MonoBehaviour {
@@ -7,6 +8,9 @@ public class Ladder : MonoBehaviour {
 
 	[SerializeField]
 	private int steps;
+
+	[SerializeField]
+	private Vector2 offset;
 
 	private readonly Path3 path = new();
 	public IReadOnlyPath<Vector3> Path => path;
@@ -20,6 +24,9 @@ public class Ladder : MonoBehaviour {
 		var currentStep = startPoint;
 		for( int i = 0; i <= steps; ++i ) {
 			var nextStep = Vector3.Lerp(startPoint, endPoint, currentDistance / totalDistance);
+			var nextOffset = offset * Math.Sign(i % 2 - 1);
+			nextStep.x += nextOffset.x;
+			nextStep.y += nextOffset.y;
 			currentStep = new Vector3(currentStep.x, nextStep.y);
 			path.Add(currentStep);
 			path.Add(nextStep);
