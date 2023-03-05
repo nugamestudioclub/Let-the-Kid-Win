7ec23d945f4729b56e0c9154466ae1224f49fe00
sudzoof <sunjoshua2@gmail.com>
2023-03-04 19:08:43 -0500

    Added a dictionary to store all dialogue values

---
+++ Assets/Scripts/DialogueDictionary.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueDictionary : Dictionary<string, Dialogue>
+{
+    public DialogueDictionary()
+    {
+        // Initialization goes here
+    }
+}
